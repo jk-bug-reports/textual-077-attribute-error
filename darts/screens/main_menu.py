496001611ccf7b1b707361bfecf46b26d21c68a7
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-08-22 23:01:36 +0200

    feat: add main menu screen (causes attribute error)

--- darts/screens/main_menu.py
+++ darts/screens/main_menu.py
@@ -1,8 +1,24 @@
+from textual import on
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Label
+from textual.widgets import Header, OptionList
+from textual.widgets.option_list import Option
 
 
 class MainMenuScreen(Screen):
+    CSS_PATH = "main_menu.tcss"
+
+    @on(OptionList.OptionSelected)
+    async def on_option_selected(self, event):
+        match event.option.id:
+            case "exit":
+                self.app.exit()
+
     def compose(self) -> ComposeResult:
-        yield Label("main menu")
+        yield Header()
+        yield OptionList(
+            Option("Start new game", id="new_game"),
+            Option("Edit players", id="edit_players"),
+            Option("Exit", id="exit"),
+            id="main_menu__option_list"
+        )
